5bd70ae53e8f85818262fb6bc24f464c9cfc97cd
amrita-20 <sona20dubey23@gmail.com>
2024-12-16 21:06:45 -0800

    cleanup

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,17 +1,17 @@
-import {ReactNode} from 'react';
+import { ReactNode } from "react";
 
 type PanelProps = {
-    title: string;
-    children: ReactNode;
-}
+  title: string;
+  children: ReactNode;
+};
 
 function Panel({ title, children }: PanelProps) {
-    return (
-        <div className='panel__wrapper'>
-            <h1 className='panel__title'>{title}</h1>
-            <p className='panel__content'>{children}</p>
-        </div>
-    )
+  return (
+    <div className="panel__wrapper">
+      <h1 className="panel__title">{title}</h1>
+      <p className="panel__content">{children}</p>
+    </div>
+  );
 }
 
 export default Panel;
--- src/feature.ts
+++ src/feature.ts
@@ -1,74 +1,83 @@
 type Features = {
-    show_product_catalog: boolean;
-    enable_product_management: boolean;
+  show_product_catalog: boolean;
+  enable_product_management: boolean;
+  show_feature_flag: boolean;
 };
 
+//mocked api 
 const SAMPLE_FEATURES: Features = {
-    show_product_catalog: false,
-    enable_product_management: true,
+  show_product_catalog: false,
+  enable_product_management: true,
+  show_feature_flag: true,
 };
+
 const TTL = 10000;
 type Cache = {
-    featureFlags: Record<string, boolean>; 
-    timestamp: number | null;
+  featureFlags: Record<string, boolean>;
+  timestamp: number | null;
 };
 
 let featureInstance: any = null;
 
-// Correctly define the cache object
 const cache: Cache = {
-    featureFlags: {}, // This matches the Cache type
-    timestamp: null,  // This matches the Cache type
+  featureFlags: {}, 
+  timestamp: null, 
 };
 
+//mocked api call
 function fetchFeatureFlags(): Promise<Features> {
-    console.log("BE calls")
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(SAMPLE_FEATURES);
-        }, 100);
-    });
+  console.log("BE calls");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(SAMPLE_FEATURES);
+    }, 100);
+  });
 }
 
 function getFeatureState(
-    featureName: keyof Features,
-    defaultValue: boolean
+  featureName: keyof Features,
+  defaultValue: boolean
 ): Promise<boolean> {
+  const isKeyPresentinCache = Object.keys(cache.featureFlags).length;
+  const presentTime = Date.now();
+  if (
+    cache.timestamp &&
+    isKeyPresentinCache &&
+    presentTime - cache.timestamp < TTL
+  ) {
+    console.log("from cache calls");
+    const isEnabled = cache.featureFlags[featureName]
+      ? cache.featureFlags[featureName]
+      : defaultValue;
+    return Promise.resolve(isEnabled);
+  }
+  if (featureInstance instanceof Promise) {
+    console.log("from promise calls");
+    return featureInstance.then((features) => {
+      const isEnabled = features[featureName]
+        ? features[featureName]
+        : defaultValue;
+      return isEnabled;
+    });
+  }
+  featureInstance = fetchFeatureFlags()
+    .then((features) => {
+      cache.featureFlags = features;
+      cache.timestamp = Date.now();
 
-    const isKeyPresentinCache = Object.keys(cache.featureFlags).length;
-    const presentTime = Date.now();
-    if (cache.timestamp && isKeyPresentinCache && (presentTime - cache.timestamp) < TTL) {
-        console.log("from cache calls")
-        const isEnabled = cache.featureFlags[featureName]
-            ? cache.featureFlags[featureName]
-            : defaultValue;
-        return Promise.resolve(isEnabled);
-    }
-    if(featureInstance instanceof Promise){
-        console.log("from promise calls")
-        return featureInstance.then((features) => {
-            const isEnabled = features[featureName]
-                ? features[featureName]
-                : defaultValue;
-            return isEnabled
-        })
-    }
-    featureInstance = fetchFeatureFlags()
-        .then((features) => {
-            cache.featureFlags = features;
-            cache.timestamp = Date.now();
-
-            const isEnabled = features[featureName]
-                ? features[featureName]
-                : defaultValue;
-            return isEnabled;
-        })
-        .catch((error) => {
-            console.error("Failed to fetch feature flags", error);
-            return defaultValue;
-        });
-    return featureInstance
+      const isEnabled = features[featureName]
+        ? features[featureName]
+        : defaultValue;
+      return isEnabled;
+    })
+    .catch((error) => {
+      console.error("Failed to fetch feature flags", error);
+      return defaultValue;
+    });
+  return featureInstance;
 }
+
+//For testing purpose
 
 // function testFeatureFlag() {
 //   getFeatureState("show_product_catalog", false).then((isEnabled) => {
